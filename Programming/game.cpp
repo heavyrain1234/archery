053ef1d59d441ecf5d9416c6ea4df374d4ef5557
Igralino <Igralino@ya.ru>
2016-10-09 23:03:33 +0300

    Изменение названия

--- Programming/game.cpp
+++ Programming/game.cpp
@@ -6,8 +6,8 @@ int game (luchnik *a_game, luchnik *b_game){
         b_game[i].udar=1.0*(rand()% (b_game[i].sila/2+2) + (b_game[i].sila/2) )* (b_game[i].hp/100);
         }
     
-    for (int i=0; i<20; i++){
-        a.game[
-    }
+    //for (int i=0; i<20; i++){
+     //   a.game[
+   // }
     return 0;
 }
