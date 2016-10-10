b1d03f6edcd7f00e38548691a01a2c29a40708af
Igralino <Igralino@ya.ru>
2016-10-10 12:23:00 +0300

    Блок снятие HP у целей

--- Programming/game.cpp
+++ Programming/game.cpp
@@ -1,13 +1,23 @@
 #include "header.hpp"
 int game (luchnik *a_game, luchnik *b_game){
-    srand (time(NULL) );
+    srand (time(NULL));
     for (int i=0; i<20; i++){
+        
         a_game[i].udar=1.0*(rand()% (a_game[i].sila/2+2) + (a_game[i].sila/2) )* (a_game[i].hp/100);
         b_game[i].udar=1.0*(rand()% (b_game[i].sila/2+2) + (b_game[i].sila/2) )* (b_game[i].hp/100);
-        }
+        
+    }
     
-    //for (int i=0; i<20; i++){
-     //   a.game[
-   // }
+    for (int i=0; i<20; i++){
+        
+        if (a_game[i].target!=-1){
+            b_game[a_game[i].target].hp-=a_game[i].udar;
+        }
+        
+        if (b_game[i].target!=-1){
+            a_game[b_game[i].target].hp-=b_game[i].udar;
+        }
+        
+    }
     return 0;
 }
